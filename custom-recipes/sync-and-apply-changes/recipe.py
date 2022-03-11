9348f97f1039ea377afbf6fd4f4507e843691724
admin <louis.dorard@dataiku.com>
2022-03-11 18:39:06 +0100

    Edited file 'custom-recipes/sync-and-apply-changes/recipe.py'

--- custom-recipes/sync-and-apply-changes/recipe.py
+++ custom-recipes/sync-and-apply-changes/recipe.py
@@ -1,23 +1,7 @@
-# Code for custom code recipe sync-and-apply-changes (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
 from dataiku.customrecipe import *
-
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
 
 input_names = get_input_names_for_role('input')
 input_datasets = [dataiku.Dataset(name) for name in input_names]
@@ -25,54 +9,17 @@ input_datasets = [dataiku.Dataset(name) for name in input_names]
 changes_names = get_input_names_for_role('changes')
 changes_datasets = [dataiku.Dataset(name) for name in changes_names]
 
+editable_names = get_output_names_for_role('editable')
+editable_datasets = [dataiku.Dataset(name) for name in editable_names]
 
+input_dataset = input_datasets[0]
+input_df = input_dataset.get_dataframe()
 
-input_A_names = get_input_names_for_role('changes')
-# The dataset objects themselves can then be created like this:
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
+changes_dataset = changes_datasets[0]
+changes_df = input_dataset.get_dataframe()
 
-# For outputs, the process is the same:
-output_A_names = get_output_names_for_role('editable')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
+editable_df = input_df
+# TODO: apply changes
 
-
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
-
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
-
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-my_variable = get_recipe_config()['parameter_name']
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-
-# Read recipe inputs
-iris = dataiku.Dataset("iris")
-iris_df = iris.get_dataframe()
-changes = dataiku.Dataset("changes")
-changes_df = changes.get_dataframe()
-
-
-iris_editable_df = iris_df
-
-
-# Write recipe outputs
-iris_editable = dataiku.Dataset("iris_editable")
-iris_editable.write_with_schema(iris_editable_df)
+editable_dataset = editable_datasets[0]
+editable_dataset.write_with_schema(editable_df)
