d0b015f50f164bfb80004064c4dae229461c5251
Louis Dorard <louis.dorard@dataiku.com>
2022-04-11 19:07:12 +0200

    [WIP] Implement join edit type

--- plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
+++ plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
@@ -4,6 +4,7 @@ from dataiku.core.sql import SQLExecutor2
 from dataiku.customwebapp import *
 from dash import dash_table, html
 from dash.dependencies import Input, Output, State
+from pandas import DataFrame
 import datetime
 
 
@@ -26,15 +27,30 @@ import datetime
 
 import os
 if (os.getenv("DKU_CUSTOM_WEBAPP_CONFIG")):
-    ref_dataset = get_webapp_config()['ref_dataset']
-    ref_key = get_webapp_config()['ref_key']
-    ext_dataset = get_webapp_config()['ext_dataset']
-    ext_key = get_webapp_config()['ext_key']
-    # TODO: use these in the rest of the code
-    # user_name = get_webapp_config()['user'] TODO: does this exist? or can we get it from an environment variable (DKU_CURRENT_USER?)
+    edit_type = get_webapp_config()['edit_type']
+    if (edit_type=="simple"):
+        input_dataset = get_webapp_config()['input_dataset']
+        input_key = get_webapp_config()['input_key']
+    elif (edit_type=="join"):
+        ref_dataset = get_webapp_config()['ref_dataset']
+        ref_key = get_webapp_config()['ref_key']
+        ref_lookup_columns = get_webapp_config()['ref_lookup_columns']
+        ext_dataset = get_webapp_config()['ext_dataset']
+        ext_key = get_webapp_config()['ext_key']
+        ext_lookup_columns = get_webapp_config()['ext_lookup_columns']
+    # user_name = get_webapp_config()['user'] IDEA: does this exist? or can we get it from an environment variable (DKU_CURRENT_USER?)
 else:
-    dataset_name = "iris"
-    unique_key = "index"
+    edit_type = "join"
+    if (edit_type=="simple"):
+        input_dataset = "iris"
+        input_key = "index"
+    elif (edit_type=="join"):
+        ref_dataset = "companies_ref"
+        ref_key = "id"
+        ref_lookup_columns = ["name", "city", "country"]
+        ext_dataset = "companies_ext"
+        ext_key = "id"
+        ext_lookup_columns = ["name", "city", "country"]
 project_key = os.getenv("DKU_CURRENT_PROJECT_KEY")
 if (not project_key or project_key==""):
     project_key = "EDITABLE"
@@ -49,39 +65,47 @@ project = client.get_project(project_key)
 
 # 3. Create change log dataset and editable dataset, if they don't already exist
 
-original_ds = dataiku.Dataset(dataset_name, project_key)
-original_df = original_ds.get_dataframe()
-connection_name = original_ds.get_config()['params']['connection'] # name of the connection to the original dataset, to use for the editable dataset too
+if (edit_type=="simple"):
+    original_ds = dataiku.Dataset(input_dataset, project_key)
+    original_df = original_ds.get_dataframe()
+    connection_name = original_ds.get_config()['params']['connection'] # name of the connection to the original dataset, to use for the editable dataset too
 
-changes_ds_name = dataset_name + "_changes"
-editable_ds_name = dataset_name + "_editable"
+    changes_ds_name = input_dataset + "_changes"
+    editable_ds_name = input_dataset + "_editable"
 
-changes_ds_creator = dataikuapi.dss.dataset.DSSManagedDatasetCreationHelper(project, changes_ds_name)
-editable_ds_creator = dataikuapi.dss.dataset.DSSManagedDatasetCreationHelper(project, editable_ds_name)
+    changes_ds_creator = dataikuapi.dss.dataset.DSSManagedDatasetCreationHelper(project, changes_ds_name)
+    editable_ds_creator = dataikuapi.dss.dataset.DSSManagedDatasetCreationHelper(project, editable_ds_name)
 
-if (not changes_ds_creator.already_exists()):
-    changes_ds_creator.with_store_into(connection="filesystem_managed")
-    changes_ds_creator.create()
-    changes_ds = dataiku.Dataset(changes_ds_name)
-    changes_ds.write_schema_from_dataframe(df=original_df)
-    
-    editable_ds_creator.with_store_into(connection=connection_name)
-    editable_ds_creator.create()
-    editable_ds = dataiku.Dataset(editable_ds_name)
-    editable_ds.write_with_schema(original_df)
-    
-    recipe_creator = dataikuapi.dss.recipe.DSSRecipeCreator("CustomCode_sync-and-apply-changes", "compute_" + editable_ds_name, project)
-    recipe = recipe_creator.create()
-    settings = recipe.get_settings()
-    settings.add_input("input", dataset_name)
-    settings.add_input("changes", changes_ds_name)
-    settings.add_output("editable", editable_ds_name)
-    settings.raw_params["customConfig"] = {"key": get_webapp_config()['key']}
-    settings.save()
-else:
-    changes_ds = dataiku.Dataset(changes_ds_name, project_key)
-    editable_ds = dataiku.Dataset(editable_ds_name, project_key)
+    if (not changes_ds_creator.already_exists()):
+        changes_ds_creator.with_store_into(connection="filesystem_managed")
+        changes_ds_creator.create()
+        changes_ds = dataiku.Dataset(changes_ds_name)
+        changes_ds.write_schema_from_dataframe(df=original_df)
+        
+        editable_ds_creator.with_store_into(connection=connection_name)
+        editable_ds_creator.create()
+        editable_ds = dataiku.Dataset(editable_ds_name)
+        editable_ds.write_with_schema(original_df)
+        
+        recipe_creator = dataikuapi.dss.recipe.DSSRecipeCreator("CustomCode_sync-and-apply-changes", "compute_" + editable_ds_name, project)
+        recipe = recipe_creator.create()
+        settings = recipe.get_settings()
+        settings.add_input("input", input_dataset)
+        settings.add_input("changes", changes_ds_name)
+        settings.add_output("editable", editable_ds_name)
+        settings.raw_params["customConfig"] = {"key": input_key}
+        settings.save()
+    else:
+        changes_ds = dataiku.Dataset(changes_ds_name, project_key)
+        editable_ds = dataiku.Dataset(editable_ds_name, project_key)
+elif (edit_type=="join"):
+    # IDEA: write code to create these datasets if they don't already exist, and the recipes to connect them
+    changes_ds = dataiku.Dataset(ref_dataset + "_" + ext_dataset + "_editlog", project_key)
+    editable_ds = dataiku.Dataset(ref_dataset + "_" + ext_dataset + "_editable", project_key)
+    ext_ds = dataiku.Dataset(ext_dataset, project_key)
+    ext_tablename = ext_ds.get_config()['params']['table'].replace("${projectKey}", project_key)
 
+changes_ds.spec_item["appendMode"] = True # make sure that we append to that dataset (and not write over it)
 editable_df = editable_ds.get_dataframe()
 cols = ([{"name": i, "id": i} for i in editable_df.columns])
 
@@ -89,14 +113,13 @@ cols = ([{"name": i, "id": i} for i in editable_df.columns])
 # 4. Initialize the SQL executor and name of table to edit
 
 executor = SQLExecutor2(connection=connection_name)
-# table_name = editable_ds.get_config()['params']['table']
-table_name = project_key + "_" + editable_ds_name
+editable_tablename = editable_ds.get_config()['params']['table'].replace("${projectKey}", project_key)
 
 
 # 5. Define the layout of the webapp
 
 app.layout = html.Div([
-    html.H3("Edit " + dataset_name),
+    html.H3("Edit " + input_dataset),
     html.Div([
         html.Div("Select a cell, type a new value, and press Enter to save."),
         html.Br(),
@@ -115,31 +138,66 @@ app.layout = html.Div([
 
 # 6. Define the callback function that updates the editable and change log when cell values get edited
 
-@app.callback(Output('output', 'children'),
+@app.callback([Output('output', 'children'),
+               Output('editable-table', 'data')],
               [State('editable-table', 'active_cell'),
-              Input('editable-table', 'data')], prevent_initial_call=True)
+               Input('editable-table', 'data')], prevent_initial_call=True)
 def update(cell_coordinates, table_data):
     row_id = cell_coordinates["row"]-1
-    idx = table_data[row_id][unique_key]
     col_id = cell_coordinates["column_id"]
-    val = table_data[row_id][col_id]
+    
+    if (edit_type=="simple"):
+        idx = table_data[row_id][input_key]
+        val = table_data[row_id][col_id]
 
-    # IDEA: surround the following with try/catch?
-    # Run update query on the editable
-    query = """UPDATE \"%s\" SET %s=%s
-            WHERE %s=%s
-            RETURNING *;
+        query = """UPDATE \"{0}\" SET {1}={2}
+            WHERE {3}={4}
             COMMIT;
-            """ % (table_name, col_id, val, unique_key, idx)
-    change_df = executor.query_to_df(query)
-    # change_df["date"] = datetime.date.today().strftime("%Y-%m-%d") TODO: first add the date field to the changes_ds schema
+            """.format(editable_tablename, col_id, val, input_key, idx)
+        executor.query_to_df(query)
+        select_change_query = """SELECT {0} FROM \"{1}\" WHERE {2}={3}""".format(col_id, editable_tablename, input_key, idx)
+
+        message = "Updated column " + str(col_id) \
+                    + " where " + input_key + " is " + idx + ". \n" \
+                    + "New value: " + str(val) + "\n\n"
+
+        change_df = executor.query_to_df(select_change_query)
+        changes_ds.write_dataframe(change_df)
+
+    elif (edit_type=="join"):
+        ref_key_value = table_data[row_id][ref_key]
+        ext_key_original_column_name = "ext_" + ext_key + "_original"
+        ext_key_reviewed_column_name = "ext_" + ext_key + "_reviewed"
+        ext_key_original_value = table_data[row_id][ext_key_original_column_name]
+        ext_key_reviewed_value = table_data[row_id][ext_key_reviewed_column_name]
+
+        if (col_id=="reviewed" & table_data[row_id]["reviewed"]=="True"):
+            message("Row is marked as reviewed, so it will be added to the edit log.")
+            
+            # We only write to the change log when the reviewed column was just set to true
+            changes_ds.write_dataframe(
+                DataFrame({ref_key: [ref_key_value], ext_key_original_column_name: [ext_key_original_value], ext_key_reviewed_column_name: [ext_key_reviewed_value]})
+                )
+
+        if (col_id==ext_key_reviewed_column_name):
+            # Retrieve values of the lookup columns from the external dataset, for this ext_idx
+            select_query = "SELECT " + ", ".join(ext_lookup_columns) + " FROM \"" + ext_tablename + "\" WHERE " + ext_key + "=" + ext_key_reviewed_value
+            select_df = executor.query_to_df(select_query)
+
+            # Update table_data with these values — note that colum names in this dataset are prefixed by "ext_"
+            for col in ext_lookup_columns: table_data[row_id]["ext_" + col] = select_df[col].iloc[0]
+
+            # Update the editable dataset with these values
+            update_query = "UPDATE \"" + editable_tablename + "\" SET " + ", ".join(["ext_" + col + "=" + str(select_df[col].iloc[0]) for col in ext_lookup_columns]) + " WHERE " + ext_key_reviewed_column_name + "=" + ext_key_reviewed_value + "; COMMIT;"
+            executor.query_to_df(update_query)
+
+            message = "Changed values of lookup columns"
+
+    
+    # change_df["date"] = datetime.date.today().strftime("%Y-%m-%d") IDEA: first add the date field to the changes_ds schema
     # change_df["user"] = "anonymous"
 
-    # Append the change to the log
-    changes_ds.spec_item["appendMode"] = True
-    changes_ds.write_dataframe(change_df)
-
-    return "Value at row " + str(row_id) + ", column " + str(col_id) + " was updated. \n" + "New value: " + str(val) + "\n\n"
+    return message, table_data
 
 
 # Uncomment the following when running the Dash app in debug mode outside of Dataiku
