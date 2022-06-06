88cc7d43cc2e3b0923387cac3e2460cb71c87884
Louis Dorard <louis.dorard@dataiku.com>
2022-06-06 12:16:46 +0200

    Remove recipe parameters

--- plugin-editable-via-webapp/custom-recipes/merge-edits-python/recipe.py
+++ plugin-editable-via-webapp/custom-recipes/merge-edits-python/recipe.py
@@ -1,5 +1,5 @@
 import dataiku
-from dataiku.customrecipe import *
+from dataiku.customrecipe import get_input_names_for_role, get_output_names_for_role
 from json import loads
 
 # when using interactive execution:
@@ -9,15 +9,9 @@ from json import loads
 import commons
 
 
-# TODO: this recipe should be created by the webapp
-webapp_json = commons.get_webapp_json(get_recipe_config()['webapp_ID'])
-# input_dataset_name = webapp_json["config"]["input_dataset"] # alternative way to get input dataset
-schema = loads(webapp_json["config"]["schema"])
-primary_key, _ = commons.get_primary_key(schema)
-
-input_names = get_input_names_for_role('input')
-input_datasets = [dataiku.Dataset(name) for name in input_names]
-input_ds = input_datasets[0]
+original_names = get_input_names_for_role('original')
+original_datasets = [dataiku.Dataset(name) for name in original_names]
+original_ds = original_datasets[0]
 
 pivoted_names = get_input_names_for_role('editlog_pivoted')
 pivoted_datasets = [dataiku.Dataset(name) for name in pivoted_names]
@@ -28,12 +22,15 @@ edited_datasets = [dataiku.Dataset(name) for name in edited_names]
 edited_ds = edited_datasets[0]
 
 # Read input data
-input_df = input_ds.get_dataframe()
+original_df = original_ds.get_dataframe()
 editlog_pivoted_df = pivoted_ds.get_dataframe()
 
 # Write output schema
-edited_ds.write_schema(input_ds.read_schema()) # otherwise column type for columns of missing values might change
+edited_ds.write_schema(original_ds.read_schema()) # otherwise column type for columns of missing values might change
+
+schema = loads(original_ds.get_config()["customFields"]["schema"])
+primary_key, _ = commons.get_primary_key(schema)
 
 # Write output data
-edited_df = commons.merge_edits(input_df, editlog_pivoted_df, primary_key)
+edited_df = commons.merge_edits(original_df, editlog_pivoted_df, primary_key)
 edited_ds.write_dataframe(edited_df)
--- plugin-editable-via-webapp/custom-recipes/pivot-editlog/recipe.py
+++ plugin-editable-via-webapp/custom-recipes/pivot-editlog/recipe.py
@@ -1,6 +1,7 @@
 import dataiku
 from dataiku.customrecipe import *
 from json import loads
+from pandas import DataFrame
 
 # when using interactive execution:
 # import sys
@@ -8,16 +9,13 @@ from json import loads
 
 import commons
 
-
-webapp_ID = get_recipe_config()['webapp_ID']
-webapp_json = commons.get_webapp_json(get_recipe_config()['webapp_ID'])
-schema = loads(webapp_json["config"]["schema"])
-editable_column_names = commons.get_editable_column_names(schema)
-
-
 editlog_names = get_input_names_for_role('editlog')
 editlog_datasets = [dataiku.Dataset(name) for name in editlog_names]
 editlog_ds = editlog_datasets[0]
+original_ds_name = DataFrame(editlog_ds.get_config()["dkuProperties"]).set_index("name").loc["original_dataset", "value"]
+original_ds = dataiku.Dataset(original_ds_name)
+schema = loads(original_ds.get_config()["customFields"]["schema"])
+editable_column_names = commons.get_editable_column_names(schema)
 
 pivoted_names = get_output_names_for_role('editlog_pivoted')
 pivoted_datasets = [dataiku.Dataset(name) for name in pivoted_names]
