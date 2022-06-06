901bc2215678d785208ef664cf3e3edb64480d16
Louis Dorard <louis.dorard@dataiku.com>
2022-06-06 17:29:00 +0200

    Fix pivot recipe so it doesn't use original dataset

--- plugin-editable-via-webapp/custom-recipes/pivot-editlog/recipe.py
+++ plugin-editable-via-webapp/custom-recipes/pivot-editlog/recipe.py
@@ -7,23 +7,17 @@ from pandas import DataFrame
 # import sys
 # sys.path.append('../../python-lib')
 
-import commons
+from commons import get_editlog_df, pivot_editlog
 
 editlog_names = get_input_names_for_role('editlog')
 editlog_datasets = [dataiku.Dataset(name) for name in editlog_names]
 editlog_ds = editlog_datasets[0]
-original_ds_name = DataFrame(editlog_ds.get_config()["dkuProperties"]).set_index("name").loc["original_dataset", "value"]
-original_ds = dataiku.Dataset(original_ds_name)
-schema = loads(original_ds.get_config()["customFields"]["schema"])
-editable_column_names = commons.get_editable_column_names(schema)
+editlog_df = get_editlog_df(editlog_ds)
 
 pivoted_names = get_output_names_for_role('editlog_pivoted')
 pivoted_datasets = [dataiku.Dataset(name) for name in pivoted_names]
 pivoted_ds = pivoted_datasets[0]
+editable_column_names = DataFrame(pivoted_ds.read_schema())["name"].to_list()[1:-1] # first and last columns are "key" and "date"
 
-# Read input data
-editlog_df = commons.get_editlog_df(editlog_ds)
-
-# Write output data
-pivoted_df = commons.pivot_editlog(editlog_df, editable_column_names)
-pivoted_ds.write_with_schema(pivoted_df)
+pivoted_df = pivot_editlog(editlog_df, editable_column_names)
+pivoted_ds.write_dataframe(pivoted_df)
