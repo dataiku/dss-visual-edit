a1c1d1e992860b5f835bee077595d46ba3e0a598
Louis Dorard <louis.dorard@dataiku.com>
2022-06-20 18:05:56 +0200

    Use CSS file hosted on S3
    GitHub repo is private, so files within it can't be accessed from anyone

--- plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
+++ plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
@@ -40,7 +40,7 @@ else:
     f_app = Flask(__name__)
     app = Dash(__name__, server=f_app)
 
-app.config.external_stylesheets = ["https://raw.githubusercontent.com/dataiku/lca/dev/plugin-editable-via-webapp/webapps/edit-dataset-records/assets/style.css", "https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"]
+app.config.external_stylesheets = ["http://s3.louisdorard.com.s3.amazonaws.com/dash_tabulator.css", "https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"]
 app.config.external_scripts = ["https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"]
 
 #%%
