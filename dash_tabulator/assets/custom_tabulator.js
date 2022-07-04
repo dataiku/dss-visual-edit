0b4732e00aba05141fa1ab8dfa8e4ce31aa3a505
Louis Dorard <louis.dorard@dataiku.com>
2022-07-04 11:36:03 +0200

    Add custom search function in JS asset

---
+++ dash_tabulator/assets/custom_tabulator.js
@@ -0,0 +1,23 @@
+window.myNamespace = Object.assign({}, window.myNamespace, {
+
+    tabulator: {
+
+        // TODO: min-max filter
+        searchFunc: function (term, values) { //search for exact matches
+            console.log(term)
+            var matches = [];
+    
+            values.forEach(function(value){
+                //value - one of the values from the value property
+    
+                if(value === term){
+                    matches.push(value);
+                }
+            });
+    
+            return matches;
+        },
+
+    }
+
+});
