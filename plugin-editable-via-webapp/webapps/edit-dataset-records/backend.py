7e24fc92d5cd3e1b9f9abf6e4c9645e8fbdaaaa6
Louis Dorard <louis.dorard@dataiku.com>
2022-07-12 18:08:15 +0200

    Refactor

--- plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
+++ plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
@@ -60,9 +60,6 @@ else:
     f_app = Flask(__name__)
     app = Dash(__name__, server=f_app)
 
-def get_last_build_date(ds_name=original_ds_name):
-    return project.get_dataset(ds_name).get_last_metric_values().get_metric_by_id("reporting:BUILD_START_DATE").get("lastValues")[0].get("computed")
-
 app.config.external_stylesheets = stylesheets
 app.config.external_scripts = scripts
 
@@ -71,7 +68,7 @@ from EditableEventSourced import EditableEventSourced
 ees = EditableEventSourced(original_ds_name, primary_keys, editable_column_names, editschema_manual)
 
 #%%
-from commons import get_user_details
+from commons import get_user_details, get_last_build_date
 user = get_user_details()
 
 #%%
@@ -87,7 +84,7 @@ def serve_layout():
     return html.Div(children=[
         html.Div(id="refresh-div", children=[
             html.Div(id="ds_update_msg", children="The original dataset has changed. Do you want to refresh? (Your edits will persist.)", className="ui warning message"),
-            html.Div(id="last_build_date", children=str(get_last_build_date()), style={"display": "none"}),
+            html.Div(id="last_build_date", children=str(get_last_build_date(original_ds_name, project)), style={"display": "none"}),
             html.Div(id="refresh-date", children="", style={"display": "none"}),
             html.Button("Refresh", id="refresh-btn", n_clicks=0, className="ui button", style={})
         ], style={"display": "none"}),
@@ -130,7 +127,7 @@ def check_data_update(n_intervals, refresh_date, refresh_div_style, last_build_d
     style_new = refresh_div_style
     style_new["display"] = "none"
     if (callback_context.triggered_id=="interval-component-iu"):
-        last_build_date_new = str(project.get_dataset(original_ds_name).get_last_metric_values().get_metric_by_id("reporting:BUILD_START_DATE").get("lastValues")[0].get("computed"))
+        last_build_date_new = str(get_last_build_date(original_ds_name, project))
         if int(last_build_date_new)>int(last_build_date):
             print("The original dataset has changed.")
             last_build_date_new_fmtd = datetime.utcfromtimestamp(int(last_build_date_new)/1000).isoformat()
