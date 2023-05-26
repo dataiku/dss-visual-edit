adf0fe4c46bfb6499e1e3ceef6eb695206ba802e
Louis Dorard <louis.dorard@dataiku.com>
2023-05-26 20:25:00 +0200

    Clean up tests

--- plugin-editable-via-webapp/webapps/edit-dataset-records/backend-tests.py
+++ plugin-editable-via-webapp/webapps/edit-dataset-records/backend-tests.py
@@ -18,40 +18,7 @@ client = dataiku.api_client()
 project = client.get_project(project_key)
 
 #%%
-ds = project.get_dataset("stakeholders_tbc_prepared")
+ds = project.get_dataset(original_ds_name)
 settings = ds.get_settings()
 definition = ds.get_definition()
 last_metrics = ds.get_last_metric_values()
-
-#%%
-from EditableEventSourced import *
-ees = EditableEventSourced(
-    original_ds_name=original_ds_name,
-    primary_keys=["id"],
-    editable_column_names=["ext_id", "reviewed", "comments"])
-
-#%%
-# when using interactive execution:
-import sys
-sys.path.append('../../python-lib')
-
-from dataiku_utils import get_rows
-from json import dumps
-def get_label(ds_name, key_column, key, label_column):
-    schema_columns = project.get_dataset(ds_name).get_schema()["columns"]
-    params = {
-        "format": "tsv-excel-header",
-        "filter": f"""strval("{key_column}")=="{key}")""",
-        "sampling": dumps({
-            "samplingMethod": "HEAD_SEQUENTIAL",
-            "maxRecords": 1
-        })
-    }
-    rows = get_rows(client, ds_name, project_key, schema_columns, params)
-    return DataFrame(columns=rows[0], data=rows[1:])[label_column].values[0]
-
-#%%
-get_label(original_ds_name, "id", "0016M00002Gw2N0", "name")
-
-# Dash cells: see backend.py starting at Dash layout definition
-# %%
