e32f76784b406d29cfe1c0efb42c4d522224b244
Louis Dorard <louis.dorard@dataiku.com>
2023-08-02 11:45:21 +0200

    [WIP] Use AIO component in backend

--- plugin-editable-via-webapp/python-lib/DataTableAIO.py
+++ plugin-editable-via-webapp/python-lib/DataTableAIO.py
@@ -1,4 +1,9 @@
-from dash import html, Input, Output, State, callback, MATCH
+import logging
+from dash import Input, Output, State, callback, MATCH
+from dash.html import Div
+from dash.dcc import Interval
+from datetime import datetime
+from commons import get_last_build_date
 from tabulator_utils import get_columns_tabulator
 from EditableEventSourced import EditableEventSourced
 from dash_tabulator import DashTabulator
@@ -7,28 +12,41 @@ import uuid
 # from dash_tabulator import DashTabulator
 
 
-class DataTableAIO(html.Div):  # html.Div will be the "parent" component
+class DataTableAIO(Div):  # Div will be the "parent" component
     class ids:
         """
         A set of functions that create pattern-matching callbacks of the subcomponents.
         """
 
-        dashtabulator = lambda aio_id: {
+        refresh_div = lambda aio_id: {
             "component": "DataTableAIO",
-            "subcomponent": "DashTabulator",
+            "subcomponent": "refresh_div",
             "aio_id": aio_id,
         }
-        div = lambda aio_id: {
+        interval = lambda aio_id: {
             "component": "DataTableAIO",
-            "subcomponent": "div",
+            "subcomponent": "interval",
+            "aio_id": aio_id,
+        }
+        dashtabulator = lambda aio_id: {
+            "component": "DataTableAIO",
+            "subcomponent": "dashtabulator",
+            "aio_id": aio_id,
+        }
+        editinfo_div = lambda aio_id: {
+            "component": "DataTableAIO",
+            "subcomponent": "editinfo_div",
             "aio_id": aio_id,
         }
 
     # Make the ids class a public class
     ids = ids
 
+    last_build_date_initial = ""
+    last_build_date_ok = False
+
     # Define the arguments of the All-in-One component
-    def __init__(self, ds_name, aio_id=None):
+    def __init__(self, ds_name, freeze_editable_columns=False, aio_id=None):
         """
         DataTableAIO is a Dash All-in-One component that is composed of a parent `html.Div`, with a `DashTabulator` component whose data comes from a Dataiku dataset.
 
@@ -44,24 +62,95 @@ class DataTableAIO(html.Div):  # html.Div will be the "parent" component
             aio_id = str(uuid.uuid4())
 
         ees = EditableEventSourced(ds_name)
-        columns = get_columns_tabulator(ees)
+        columns = get_columns_tabulator(ees, freeze_editable_columns)
+        self.project = ees.project
+
+        try:
+            self.last_build_date_initial = get_last_build_date(ds_name, self.project)
+            self.last_build_date_ok = True
+        except:
+            self.last_build_date_initial = ""
+            self.last_build_date_ok = False
 
         # Define the component's layout
         super().__init__(
             [
+                Div(
+                    id="self.ids.refresh_div(aio_id)",
+                    children=[
+                        Div(
+                            id="data-refresh-message",
+                            children="The original dataset has changed, please refresh this page to load it here. (Your edits are safe.)",
+                            style={"display": "inline"},
+                        ),
+                        Div(
+                            id="last-build-date",
+                            children=str(self.last_build_date_initial),
+                            style={"display": "none"},
+                        ),  # when the original dataset was last built
+                    ],
+                    className="ui compact warning message",
+                    style={"display": "none"},
+                ),
+                Interval(
+                    id="self.ids.interval(aio_id)",
+                    interval=10 * 1000,  # in milliseconds
+                    n_intervals=0,
+                ),
                 DashTabulator(
                     id=self.ids.dashtabulator(aio_id),
                     datasetName=ds_name,
                     columns=columns,
                     data=ees.get_edited_df().to_dict("records"),
                 ),
-                html.Div(id=self.ids.div(aio_id), children="Info zone for tabulator"),
+                Div(
+                    id=self.ids.editinfo_div(aio_id), children="Info zone for tabulator"
+                ),
             ]
         )
 
-    # Define the component's callbacks
+    self.data_fresh = True
+
     @callback(
-        Output(ids.div(MATCH), "children"),
+        [
+            Output("refresh-div", "style"),
+            Output("last-build-date", "children"),
+        ],  # TODO: fix ids
+        [
+            # Changes in the Inputs trigger the callback
+            Input("interval-component-iu", "n_intervals"),
+            # Changes in States don't trigger the callback
+            State("refresh-div", "style"),
+            State("last-build-date", "children"),
+        ],
+        prevent_initial_call=True,
+    )
+    def toggle_refresh_div_visibility(n_intervals, refresh_div_style, last_build_date):
+        """
+        Toggle visibility of refresh div, when the interval component fires: check last build date of original dataset and if it's more recent than what we had, display the refresh div
+        """
+        style_new = refresh_div_style
+        if self.last_build_date_ok:
+            last_build_date_new = str(get_last_build_date(ds_name, self.project))
+            if int(last_build_date_new) > int(last_build_date):
+                logging.info("The original dataset has changed.")
+                last_build_date_new_fmtd = datetime.utcfromtimestamp(
+                    int(last_build_date_new) / 1000
+                ).isoformat()
+                last_build_date_fmtd = datetime.utcfromtimestamp(
+                    int(last_build_date) / 1000
+                ).isoformat()
+                logging.info(
+                    f"""Last build date: {last_build_date_new} ({last_build_date_new_fmtd}) — previously {last_build_date} ({last_build_date_fmtd})"""
+                )
+                style_new["display"] = "block"
+                self.data_fresh = False
+        else:
+            last_build_date_new = last_build_date
+        return style_new, last_build_date_new
+
+    @callback(
+        Output(ids.editinfo_div(MATCH), "children"),
         Input(ids.dashtabulator(MATCH), "cellEdited"),
         State(ids.dashtabulator(MATCH), "datasetName"),
         prevent_initial_call=True,
--- plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
+++ plugin-editable-via-webapp/webapps/edit-dataset-records/backend.py
@@ -5,27 +5,23 @@
 # This code is structured as follows:
 # 0. Imports and variable initializations
 # 1. Get webapp parameters (original dataset, primary keys, editable columns, linked records...)
-# 2. Instantiate editable event-sourced dataset
 # 3. Define webapp layout and components
 
 # 0. Imports and variable initializations
 ###
 
 import logging
-from datetime import datetime
 from os import getenv
 
 import dataiku
-from commons import get_last_build_date, get_user_identifier
+from commons import get_user_identifier
 from dash import Dash, Input, Output, State, dcc, html
 from dataiku_utils import get_dataframe_filtered
 from EditableEventSourced import EditableEventSourced
+from DataTableAIO import DataTableAIO
 from flask import Flask, current_app, jsonify, make_response, request
 from linked_records_blueprint import linked_records_blueprint
-from tabulator_utils import get_columns_tabulator, get_values_from_df
 from webapp_config_utils import get_linked_records
-
-import dash_tabulator
 
 stylesheets = ["https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"]
 scripts = [
@@ -33,11 +29,7 @@ scripts = [
     "https://code.jquery.com/jquery-3.5.1.min.js",  # used by inline javascript code found in tabulator_utils (__get_column_tabulator_formatter_linked_record__)
     "https://cdn.jsdelivr.net/npm/luxon@3.0.4/build/global/luxon.min.js",
 ]
-
-client = dataiku.api_client()
-project_key = getenv("DKU_CURRENT_PROJECT_KEY")
-project = client.get_project(project_key)
-
+logger = logging.getLogger(__name__)
 
 # 1. Get webapp parameters
 ###
@@ -60,10 +52,10 @@ if getenv("DKU_CUSTOM_WEBAPP_CONFIG"):
     params = get_webapp_config()
 
     if bool(params.get("debug_mode")):
-        logging.basicConfig(level=logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.INFO)
-    logging.info("Webapp is being run in Dataiku")
+        logger.setLevel(logging.INFO)
+    logger.info("Webapp is being run in Dataiku")
 
     from json import loads
 
@@ -77,8 +69,8 @@ if getenv("DKU_CUSTOM_WEBAPP_CONFIG"):
     server.register_blueprint(linked_records_blueprint)
 
 else:
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Webapp is being run outside of Dataiku")
+    logger.setLevel(logging.DEBUG)
+    logger.info("Webapp is being run outside of Dataiku")
     run_context = "local"
     info_display = "block"
 
@@ -109,9 +101,9 @@ linked_records_count = params.get("linked_records_count")
 linked_records = get_linked_records(params, linked_records_count)
 authorized_users = params.get("authorized_users")
 
+# TODO: replace this with a call to initialize custom fields based on the parameters below
 ees = EditableEventSourced(
     original_ds_name=original_ds_name,
-    project_key=project_key,
     primary_keys=primary_keys,
     editable_column_names=editable_column_names,
     linked_records=linked_records,
@@ -120,62 +112,17 @@ ees = EditableEventSourced(
 )
 
 
-columns = get_columns_tabulator(ees, freeze_editable_columns)
-
-last_build_date_initial = ""
-last_build_date_ok = False
-
-
 def serve_layout():  # This function is called upon loading/refreshing the page in the browser
-    global last_build_date_initial, last_build_date_ok
-    try:
-        last_build_date_initial = get_last_build_date(original_ds_name, project)
-        last_build_date_ok = True
-    except:
-        last_build_date_initial = ""
-        last_build_date_ok = False
-
     # get user's email and check if it's in authorized_users; if not, return a div which says "unauthorized access"
     user_id = get_user_identifier()
     if not authorized_users or user_id in authorized_users:
         return html.Div(
-            children=[
-                html.Div(
-                    id="refresh-div",
-                    children=[
-                        html.Div(
-                            id="data-refresh-message",
-                            children="The original dataset has changed, please refresh this page to load it here. (Your edits are safe.)",
-                            style={"display": "inline"},
-                        ),
-                        html.Div(
-                            id="last-build-date",
-                            children=str(last_build_date_initial),
-                            style={"display": "none"},
-                        ),  # when the original dataset was last built
-                    ],
-                    className="ui compact warning message",
-                    style={"display": "none"},
-                ),
-                dcc.Interval(
-                    id="interval-component-iu",
-                    interval=10 * 1000,  # in milliseconds
-                    n_intervals=0,
-                ),
-                dash_tabulator.DashTabulator(
-                    id="datatable",
-                    datasetName=original_ds_name,
-                    columns=columns,
-                    data=ees.get_edited_df().to_dict(
-                        "records"
-                    ),  # this gets the most up-to-date edited data
-                    groupBy=group_column_names,
-                ),
-                html.Div(
-                    id="edit-info",
-                    children="Info zone for tabulator",
-                    style={"display": info_display},
-                ),
+            [
+                DataTableAIO(
+                    aio_id="dt",
+                    ds_name=original_ds_name,
+                    freeze_editable_columns=freeze_editable_columns,
+                )
             ]
         )
     else:
@@ -183,77 +130,6 @@ def serve_layout():  # This function is called upon loading/refreshing the page 
 
 
 app.layout = serve_layout
-
-data_fresh = True
-
-
-@app.callback(
-    [Output("refresh-div", "style"), Output("last-build-date", "children")],
-    [
-        # Changes in the Inputs trigger the callback
-        Input("interval-component-iu", "n_intervals"),
-        # Changes in States don't trigger the callback
-        State("refresh-div", "style"),
-        State("last-build-date", "children"),
-    ],
-    prevent_initial_call=True,
-)
-def toggle_refresh_div_visibility(n_intervals, refresh_div_style, last_build_date):
-    """
-    Toggle visibility of refresh div, when the interval component fires: check last build date of original dataset and if it's more recent than what we had, display the refresh div
-    """
-    global last_build_date_ok
-    style_new = refresh_div_style
-    if last_build_date_ok:
-        last_build_date_new = str(get_last_build_date(original_ds_name, project))
-        if int(last_build_date_new) > int(last_build_date):
-            logging.info("The original dataset has changed.")
-            last_build_date_new_fmtd = datetime.utcfromtimestamp(
-                int(last_build_date_new) / 1000
-            ).isoformat()
-            last_build_date_fmtd = datetime.utcfromtimestamp(
-                int(last_build_date) / 1000
-            ).isoformat()
-            logging.info(
-                f"""Last build date: {last_build_date_new} ({last_build_date_new_fmtd}) — previously {last_build_date} ({last_build_date_fmtd})"""
-            )
-            style_new["display"] = "block"
-            data_fresh = False
-    else:
-        last_build_date_new = last_build_date
-    return style_new, last_build_date_new
-
-
-@app.callback(
-    Output("edit-info", "children"),
-    Input("datatable", "cellEdited"),
-    prevent_initial_call=True,
-)
-def add_edit(cell):
-    """
-    Record edit in editlog, once a cell has been edited
-
-    If the cell is in the Reviewed column, we also update values for all other editable columns in the same row (except Comments). The values in these columns are generated by the upstream data flow and subject to change. We record them, in case the user didn't edit them before marking the row as reviewed.
-    """
-    info = ""
-    if cell["field"] == "Reviewed":
-        for col in editable_column_names:
-            if col != "Comments":
-                info += (
-                    ees.update_row(
-                        primary_keys=cell[
-                            "row"
-                        ],  # contains values for primary keys — and other columns too, but they'll be discarded
-                        column=col,
-                        value=cell["row"][col],
-                    )
-                    + "\n"
-                )
-    else:
-        info = ees.update_row(
-            primary_keys=cell["row"], column=cell["field"], value=cell["value"]
-        )
-    return info
 
 
 # Dummy endpoints
@@ -279,4 +155,4 @@ def static_page():
     return current_app.send_static_file("values_url.html")
 
 
-logging.info("Webapp OK")
+logger.info("Webapp OK")
