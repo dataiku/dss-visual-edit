f8db100e599f01b4c9046d74a480d263bbdffbb8
Louis Dorard <louis.dorard@dataiku.com>
2023-12-20 15:41:23 +0100

    Fix issue where linked records count is undefined

--- plugin-editable-via-webapp/python-lib/webapp_config_utils.py
+++ plugin-editable-via-webapp/python-lib/webapp_config_utils.py
@@ -3,8 +3,9 @@ def get_linked_records(params, linked_records_count):
     Get linked records from webapp parameters, as a list of dictionaries
     """
     linked_records = []
-    if (linked_records_count > 0):
-        for c in range(1, linked_records_count+1):
+    # if linked_records_count is not None and is positive
+    if linked_records_count and linked_records_count > 0:
+        for c in range(1, linked_records_count + 1):
             name = params.get(f"linked_record_name_{c}")
             ds_name = params.get(f"linked_record_ds_name_{c}")
             ds_key = params.get(f"linked_record_key_{c}")
@@ -20,7 +21,7 @@ def get_linked_records(params, linked_records_count):
                     "ds_name": ds_name,
                     "ds_key": ds_key,
                     "ds_label": ds_label,
-                    "ds_lookup_columns": ds_lookup_columns
+                    "ds_lookup_columns": ds_lookup_columns,
                 }
             )
     return linked_records
