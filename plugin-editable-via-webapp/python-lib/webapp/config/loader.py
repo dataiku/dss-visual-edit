a68182ddbd3f49a91b610428231ad2cf415172ca
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-04-22 19:42:19 +0200

    Inline method to parse config in WebAppConfig.

--- plugin-editable-via-webapp/python-lib/webapp/config/loader.py
+++ plugin-editable-via-webapp/python-lib/webapp/config/loader.py
@@ -1,7 +1,6 @@
 from dataiku.customwebapp import get_webapp_config
 from json import loads, load
 from os import getenv
-from webapp_config_utils import get_linked_records
 
 
 class WebAppConfig:
@@ -33,7 +32,9 @@ class WebAppConfig:
         self.freeze_editable_columns = config.get("freeze_editable_columns")
         self.group_column_names = config.get("group_column_names")
         self.linked_records_count = config.get("linked_records_count")
-        self.linked_records = get_linked_records(config, self.linked_records_count)
+        self.linked_records = self.__get_linked_records__(
+            config, self.linked_records_count
+        )
         self.authorized_users = config.get("authorized_users")
 
         self.project_key = getenv("DKU_CURRENT_PROJECT_KEY")
@@ -41,3 +42,31 @@ class WebAppConfig:
             raise Exception(
                 "No project key configured. DKU_CURRENT_PROJECT_KEY environment variable is expected to be set."
             )
+
+    def __get_linked_records__(self, params, linked_records_count):
+        """
+        Get linked records from webapp parameters, as a list of dictionaries
+        """
+        linked_records = []
+        # if linked_records_count is not None and is positive
+        if linked_records_count and linked_records_count > 0:
+            for c in range(1, linked_records_count + 1):
+                name = params.get(f"linked_record_name_{c}")
+                ds_name = params.get(f"linked_record_ds_name_{c}")
+                ds_key = params.get(f"linked_record_key_{c}")
+                ds_label = params.get(f"linked_record_label_column_{c}")
+                ds_lookup_columns = params.get(f"linked_record_lookup_columns_{c}")
+                if not ds_label:
+                    ds_label = ds_key
+                if not ds_lookup_columns:
+                    ds_lookup_columns = []
+                linked_records.append(
+                    {
+                        "name": name,
+                        "ds_name": ds_name,
+                        "ds_key": ds_key,
+                        "ds_label": ds_label,
+                        "ds_lookup_columns": ds_lookup_columns,
+                    }
+                )
+        return linked_records
