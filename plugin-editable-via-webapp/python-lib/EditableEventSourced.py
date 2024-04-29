4d2b587d20fdaf1feffdb59349bba3f8b95160fd
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-04-29 17:56:35 +0200

    Change logging message.

--- plugin-editable-via-webapp/python-lib/EditableEventSourced.py
+++ plugin-editable-via-webapp/python-lib/EditableEventSourced.py
@@ -67,7 +67,7 @@ class EditableEventSourced:
             )
             self.webapp_url_public = f"/public-webapps/{self.project_key}/{webapp_id}/"
         except Exception:
-            logging.exception("Failed to init webapp url.")
+            logging.exception("Failed to retrieve webapp url.")
             self.webapp_url = None
             self.webapp_url_public = "/"
 
