8c115e183fa68e080a0419b1095a0a7c248ab5c6
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-04-30 20:48:33 +0200

    Fix debug mode deserialization in the context of DSS.

--- plugin-editable-via-webapp/python-lib/webapp/logging/setup.py
+++ plugin-editable-via-webapp/python-lib/webapp/logging/setup.py
@@ -1,26 +1,43 @@
+from __future__ import annotations
 from os import getenv
 import logging
 from typing import Union
 from dataiku.customwebapp import get_webapp_config
 
 
-def __bool__(val: Union[str, None]) -> bool:
-    if val is None:
-        return False
-    return val.lower() == "true"
+def __bool__(val: Union[str, None]) -> bool | None:
+    if isinstance(val, bool):
+        return val
+    elif isinstance(val, str):
+        if val.lower() == "true":
+            return True
+        elif val.lower() == "false":
+            return False
+    return None
 
 
 def __is_running_in__dss__() -> bool:
     return getenv("DKU_CUSTOM_WEBAPP_CONFIG") is not None
 
 
-if __is_running_in__dss__():
-    if __bool__(get_webapp_config().get("debug_mode")):
+def __setup__():
+    debug_mode = __bool__(
+        get_webapp_config().get("debug_mode")
+        if __is_running_in__dss__()
+        else getenv("DEBUG_MODE")
+    )
+
+    if debug_mode:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
-else:
-    if __bool__(getenv("DEBUG_MODE")):
-        logging.basicConfig(level=logging.DEBUG)
+
+    if debug_mode is None:
+        logging.warning(
+            "Expected a boolean for debug_mode value. Default to no debug mode."
+        )
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.info(f"DEBUG MODE: {debug_mode}")
+
+
+__setup__()
