9912918418b493738c8ac21eca9d6f0fff3021b5
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-05-14 16:05:00 +0200

    Change plugin identifier [sc-181386]

--- dss-plugin-visual-edit/python-lib/webapp_utils.py
+++ dss-plugin-visual-edit/python-lib/webapp_utils.py
@@ -9,8 +9,12 @@ def call_rest_api(path):
     PORT = dataiku.base.remoterun.get_env_var("DKU_BASE_PORT")
     if PORT is None:
         PORT = "11200"
-    BASE_API_URL = "http://127.0.0.1:" + PORT + \
-        "/public/api/projects/" + getenv("DKU_CURRENT_PROJECT_KEY")
+    BASE_API_URL = (
+        "http://127.0.0.1:"
+        + PORT
+        + "/public/api/projects/"
+        + getenv("DKU_CURRENT_PROJECT_KEY")
+    )
     return loads(
         requests.get(
             url=BASE_API_URL + path,
@@ -27,7 +31,7 @@ def get_webapp_json(webapp_ID):
 def find_webapp_id(original_ds_name):
     webapps_df = DataFrame(call_rest_api("/webapps/"))
     webapps_edit_df = webapps_df[
-        webapps_df["type"] == "webapp_editable-via-webapp_edit-dataset-records"
+        webapps_df["type"] == "webapp_visual-edit_visual-edit"
     ]
     webapps_edit_df["original_ds_name"] = webapps_edit_df.apply(
         lambda row: get_webapp_json(row["id"]).get("config").get("original_dataset"),
