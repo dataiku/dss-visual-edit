cf14bdea399caa05093b4f11e94da968244977fc
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-07-01 14:07:16 +0200

    Change workflow and code to handle several webapps.

--- dss-plugin-visual-edit/tests/features/steps/url_builder.py
+++ dss-plugin-visual-edit/tests/features/steps/url_builder.py
@@ -4,7 +4,13 @@ import os
 
 from dataikuapi.dss.webapp import DSSWebApp
 
-webapp_run_id = os.getenv("DKU_WEBAPP_RUN_ID")
+webapp_run_ids = os.getenv("DKU_WEBAPP_RUN_IDS")
+assert webapp_run_ids, "Expected DKU_WEBAPP_RUN_IDS to be set."
+webapp_id_to_run_id = {}
+for tuple in list(webapp_run_ids.split(",")):
+    arr = tuple.split(":")
+    webapp_id_to_run_id = webapp_id_to_run_id | {arr[0]: arr[1]}
+
 webapp_cookie = os.getenv("DKU_WEBAPP_COOKIE")
 
 
@@ -15,6 +21,7 @@ def get_cookie() -> str:
 
 
 def create_webapp_backend_url(webapp: DSSWebApp):
-    if webapp_run_id:
-        return f"/web-apps-backends/{webapp.project_key}/{webapp_run_id}/"
-    raise Exception("Web app run id must be specified in env var DKU_WEBAPP_RUN_ID.")
+    match = webapp_id_to_run_id.get(webapp.webapp_id, "")
+    if not match:
+        raise Exception(f"Missing configuration for web app id {webapp.webapp_id}.")
+    return f"/web-apps-backends/{webapp.project_key}/{match}/"
