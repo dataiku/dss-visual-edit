01dbcf86fb506ee0313990b4705a4f70a05f9d7d
Fabien Daoulas <fabien.daoulas@dataiku.com>
2024-08-13 17:28:30 +0200

    Add tests for lookup.

--- dss-plugin-visual-edit/tests/features/steps/steps.py
+++ dss-plugin-visual-edit/tests/features/steps/steps.py
@@ -54,6 +54,38 @@ def assert_label(ctx: AugmentedBehaveContext, key: str, ds_name: str, label: str
     assert response.text == label
 
 
+@behave.then('the lookup result for term "{term}" in dataset "{ds_name}"')
+def assert_lookup(ctx: AugmentedBehaveContext, term: str, ds_name: str):
+    webapp = get_webapp(ctx, None, None)
+    assert webapp
+
+    url = create_api_url(ctx, f"lookup/{ds_name}?term={term}")
+
+    response = get(url, cookies=get_cookie_as_dict(ctx))
+
+    response.raise_for_status()
+    result = response.json()
+
+    if ctx.table and len(ctx.table.rows):
+        assert len(result) == len(ctx.table.rows)
+
+        value_type = type(result[0]["value"])
+
+        converted_rows = []
+        for row in ctx.table.rows:
+            dict_row = row.as_dict()
+            if value_type is int:
+                dict_row["value"] = int(row["value"])
+            elif value_type is float:
+                dict_row["value"] = float(row["value"])
+
+            converted_rows.append(dict_row)
+
+        assert result == converted_rows
+    else:
+        assert result == []
+
+
 @behave.then("editing rows as such is unauthorized")
 def unauthorized_edit_rows(ctx: AugmentedBehaveContext):
     webapp = get_webapp(ctx, None, None)
