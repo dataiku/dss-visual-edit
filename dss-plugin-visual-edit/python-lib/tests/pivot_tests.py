15e9a3af746eaa31de819d36c7780888486bbba9
Louis Dorard <louis.dorard@dataiku.com>
2025-05-15 14:54:57 +0200

    Update tests

--- dss-plugin-visual-edit/python-lib/tests/pivot_tests.py
+++ dss-plugin-visual-edit/python-lib/tests/pivot_tests.py
@@ -1,13 +1,27 @@
+#%%
+import sys
+sys.path.append('../../python-lib')
+
 from __future__ import annotations
-from os import getenv
+from os import getenv, environ
 from dataiku import Dataset
 from commons import replay_edits, apply_edits_from_df
 
-original_ds_name: str | None = getenv("ORIGINAL_DATASET")
+environ["DKU_CURRENT_PROJECT_KEY"] = "EX_ENTITY_RESOLUTION"
+environ["ORIGINAL_DATASET"] = "match_suggestions_prepared"
+
+original_ds_name: str | None = environ.get("ORIGINAL_DATASET")
 original_ds = Dataset(original_ds_name)
-editlog_ds = Dataset("" if original_ds_name is None else original_ds_name + "_editlog")
+editlog_ds = Dataset(original_ds_name + "_editlog")
 primary_keys = editlog_ds.get_config()["customFields"]["primary_keys"]
 editable_column_names = editlog_ds.get_config()["customFields"]["editable_column_names"]
-edits_df = replay_edits(editlog_ds, primary_keys, editable_column_names)
+validation_column_required = editlog_ds.get_config()["customFields"]["validation_column_required"]
+notes_column_required = editlog_ds.get_config()["customFields"]["notes_column_required"]
+
+#%% Replay edits
+edits_df = replay_edits(editlog_ds, primary_keys, editable_column_names, validation_column_required, notes_column_required)
+
+#%% Apply edits to original dataset
 edited_df = apply_edits_from_df(original_ds, edits_df)
-print(edited_df)
+
+# %%
